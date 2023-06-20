615850d926ac72724d3da8d5022d46d4bab8b184
Srabo93 <a.s-dev@web.de>
2023-06-20 23:57:19 +0200

    configured keymaps, full js/typescript environment with dap

--- mappings.lua
+++ mappings.lua
@@ -1,8 +1,4 @@
 -- Mapping data with "desc" stored directly by vim.keymap.set().
---
--- Please use this mappings table to set keyboard mapping since this is the
--- lower level configuration and more robust one. (which-key will
--- automatically pick-up stored data by this setting.)
 return {
   -- first key is the mode
   n = {
@@ -11,18 +7,28 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<tab>"] = {
+      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+      desc = "Next buffer",
+    },
+    ["<S-tab>"] = {
+      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+      desc = "Previous buffer",
+    },
+    ["<leader>tt"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
+    ["<C-'>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
   },
   t = {
-    -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
+    ["<leader>tt"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
+    ["<C-'>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
   },
 }
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,7 +3,30 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+                "vim",
+    "lua",
+    "html",
+    "css",
+    "scss",
+    "javascript",
+    "typescript",
+    "tsx",
+    "c",
+    "markdown",
+    "markdown_inline",
+    "rust",
+    "sql",
+    "dockerfile",
+    "dot",
+    "gitignore",
+    "graphql",
+    "jsdoc",
+    "json",
+    "php",
+    "phpdoc",
+    "toml",
+    "twig",
+    "yaml",
     })
   end,
 }
