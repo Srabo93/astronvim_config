85013c1135cb735d1e4a1d19c81286b69a66fc23
srabo93 <a.s-dev@web.de>
2023-08-29 16:16:41 +0200

    php linting/formatting

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -19,6 +19,12 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         "stylua",
+        "php-cs-fixer",
+        "prettierd",
+        "eslint_d",
+        "phpstan",
+        "phpcs",
+        "twigcs",
       })
     end,
   },
